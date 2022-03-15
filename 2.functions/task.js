c6081752517557b9775cfdfe5aa5369cb0bfd2ec
Anna Popova <panasenko-anna.8@mail.ru>
2022-03-15 14:57:16 +0300

    modified task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,51 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
-  // Ваш код
+  for (let item of arr) {
+    if (item > max) max = item;
+    if (item < min) min = item;
+    sum += item
+  }
+
+  let avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let item of arr) {
+   sum += item;
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+   let max = 0;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+   for (let item of arrOfArr) {
+   let sum = func(item);
+   if (sum > max)  max = sum;
+   }
+   
+   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+   let min = arr[0];
+   let max = arr[0];
+ 
+   for (let item of arr) {
+     if (item < min) min = item;
+     if (item > max) max = item;
+   }
+ 
+   return max - min;  
 }
