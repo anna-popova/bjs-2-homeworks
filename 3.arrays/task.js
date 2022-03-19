d044950a6aba6141512ef76fddd93e6dfebfe9b1
Anna Popova <panasenko-anna.8@mail.ru>
2022-03-19 23:04:15 +0300

    add all solutions

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,25 @@
 function compareArrays(arr1, arr2) {
 
-	return arr1.length == arr2.length && arr1.every(isSimilar, arr2);
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 
-	function isSimilar(item, index) {
-		return item === this[index];
-	}
+	//решение с циклом
+	// let result;
+	// for (let i = 0; i < arr1.length; i++) {
+	// 	for (let y = 0; y < arr2.length; y++) {
+	// 	  if (arr1.length === arr2.length && arr1[i] === arr2[y]) {
+	// 		 result = true;
+	// 	  } else {
+	// 		 result = false;
+	// 	  }
+	// 	}
+	//  }
+	//  return result;
+
+	//решение с this
+	// return arr1.length == arr2.length && arr1.every(isSimilar, arr2);
+	// function isSimilar(item, index) {
+	// 	return item === this[index];
+	// }
 }
 
 function advancedFilter(arr) {
