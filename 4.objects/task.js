a928c692bfaae7a0502ad4c65357775eafa2107b
Anna Popova <panasenko-anna.8@mail.ru>
2022-03-25 16:48:39 +0300

    fix problems

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,7 +1,7 @@
 function Student(name, gender, age) {
-	this.name = name,
-	this.gender = gender,
-	this.age = age
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -33,20 +33,3 @@ Student.prototype.exclude = function(reason) {
 	delete this.marks;
 	this.excluded = reason;
 }
-
-let student1 = new Student("Tony", "male", 37);
-student1.setSubject("Algebra");
-student1.addMark(5);
-student1.addMark(4);
-student1.addMark(5);
-console.log(student1.getAverage()); //4.6666
-console.log(student1);
-// {age: 37, gender: "male", marks: [5, 4, 5], name: "Tony", subject: "Algebra"}
-let student2 = new Student("Buzz", "female", 35);
-student2.setSubject("Geometry");
-student2.addMark(2);
-student2.addMark(3);
-student2.addMark(2);
-student2.exclude('low grades')
-console.log(student2)
-// {name: "Buzz", gender: "female", age: 35, excluded: "low grades"}
