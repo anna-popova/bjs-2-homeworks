d1a442c2c05aafcb60ee42bf0c1662215e45554b
Anna Popova <panasenko-anna.8@mail.ru>
2022-04-01 13:30:32 +0300

    add solution for tasks part 2

--- 7.async/task.js
+++ 7.async/task.js
@@ -9,7 +9,7 @@ class AlarmClock {
 		 throw new Error('Невозможно идентифицировать будильник. Параметр id не передан');
 	  } 
 	  
-	  if(this.alarmCollection.find(item => item.id === id)) {
+	  if(this.alarmCollection.find(alarm => alarm.id === id)) {
 		 console.error('Будильник с таким id уже существует');
 		 return this.alarmCollection;
 	  } 
@@ -19,26 +19,40 @@ class AlarmClock {
 	}
  
 	removeClock(id) {
-	  this.alarmCollection = this.alarmCollection.filter(item => item.id !== id);
- 
-	  
+	  this.alarmCollection = this.alarmCollection.filter(alarm => alarm.id !== id);
 	}
  
+	getCurrentFormattedTime() {
+	  let date = new Date();
+	  return `${date.getHours()}:${date.getMinutes()}`
+	}
  
+	start() {
+	  function checkClock(alarm) {
+		 if(this.alarmCollection.filter(alarm => alarm.time === this.getCurrentFormattedTime)) {
+			return this.alarmCollection.callback();
+		 } 
+	  }
+	}
 	
  }
  
  const alarm = new AlarmClock();
  // console.log(alarm);
  
- alarm.addClock('09:10', () => conslole.log('Пора вставать'), 1);
- alarm.addClock('09:20', () => conslole.log('Пора вставать'), 1); //Будильник с таким id уже существует
- alarm.addClock('09:20', () => conslole.log('Пора вставать'), 2);
- alarm.addClock('09:30', () => conslole.log('Пора вставать'), 3);
- alarm.addClock('09:30', () => conslole.log('Пора вставать')); //ошибка: нет id
- alarm.addClock('09:40', () => conslole.log('Пора вставать'), 4);
- console.log(alarm);
+ alarm.addClock('10:27', () => conslole.log('Пора вставать'), 1);
+ // alarm.addClock('10:26', () => conslole.log('Пора вставать'), 1); //Будильник с таким id уже существует
+ alarm.addClock('10:27', () => conslole.log('Скорее вставай'), 2);
+ alarm.addClock('10:28', () => conslole.log('Уже надо встать!'), 3);
+ alarm.addClock('10:29', () => conslole.log('Уже надо встать!')); //ошибка: нет id
+ alarm.addClock('10:30', () => conslole.log('Вставай, а то проспишь!'), 4);
+ // console.log(alarm);
  
  alarm.removeClock(3);
+ alarm.removeClock(4);
  console.log(alarm);
+ 
+ // console.log(alarm.getCurrentFormattedTime());
+ console.log(alarm.start());
+ 
  
