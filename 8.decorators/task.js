3ad63373cf655bf8d0517c754294695be160b831
Anna Popova <panasenko-anna.8@mail.ru>
2022-04-06 11:59:01 +0300

    add solution for task 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,26 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	let cache = [];
+
+	return function(...args) {
+		const hash = args.join(',');
+		let idx = cache.findIndex( (item) => item.hash === hash );
+	  // console.log(idx);
+
+		if (idx !== -1 ) {
+			console.log("Из кэша: " + cache[idx].value);
+			return "Из кэша: " + cache[idx].value;
+		}
+
+		let result = func(...args);
+		cache.push({hash: hash, value: result});
+	  // console.log(cache);
+
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result; 
+	}
 }
 
 
